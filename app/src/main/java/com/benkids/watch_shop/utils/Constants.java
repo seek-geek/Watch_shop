cc9f53360c37d0270cfb5bbcc0b99b23246a2ca2
seek-geek <2363550687@qq.com>
2015-11-24 20:48:24 +0800

    添加了hot top布局 并且初始化部分

--- app/src/main/java/com/benkids/watch_shop/utils/Constants.java
+++ app/src/main/java/com/benkids/watch_shop/utils/Constants.java
@@ -7,6 +7,10 @@ public interface Constants {
 
     interface URL{
         String AD_IMAGE_URL = "http://wb.app.wbiao.cn/v1/home/banners";
+
+        //选表hot——标签
+        String TABLE_HOTTag_URL = "http://wb.app.wbiao.cn/v1/xuanbiao/hottags";
+        String TABLE_HOTBRANDS_URL = "http://wb.app.wbiao.cn/v1/xuanbiao/hotbrands";
     }
 
     interface BUNDLE_KEY{
